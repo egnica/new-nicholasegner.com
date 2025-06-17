5e14f059ae9e2c7925d6b4f9a0d44e8c62b36cf5
Nicholas Egner <nicholasegner@Nicholass-MacBook-Pro.local>
2025-06-17 10:14:33 -0500

    Your commit message here

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -2,12 +2,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
 
-import React from 'react'
+import React from "react";
 
 function header() {
-  return (
-    <div>header</div>
-  )
+  return <div>header</div>;
 }
 
-export default header
+export default header;
