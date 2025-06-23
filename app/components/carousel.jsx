aed8edb1c0ce8a2f0497d072c050dad58da7ba4a
Nicholas Egner <nick@nicholasegner.com>
2025-06-23 07:26:47 -0500

    carousel images

---
+++ app/components/carousel.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import styles from "../page.module.css";
+
+function Carousel({ imageArray }) {
+  const ArrayLength = imageArray.length;
+  const [changeIndex, setChangeIndex] = useState(0);
+  const [clicked, setClicked] = useState(null);
+
+  useEffect(() => {
+    if (!clicked) {
+      const interval = setInterval(() => {
+        setChangeIndex((prevIndex) =>
+          prevIndex + 1 >= ArrayLength ? 0 : prevIndex + 1
+        );
+      }, 4000);
+
+      return () => clearInterval(interval);
+    }
+  }, [clicked, ArrayLength]);
+  const goPrev = () => {
+    setClicked(true);
+    setChangeIndex((prevIndex) =>
+      prevIndex - 1 < 0 ? ArrayLength - 1 : prevIndex - 1
+    );
+  };
+  const goNext = () => {
+    setClicked(true);
+    setChangeIndex((prevIndex) => (prevIndex + 1) % ArrayLength);
+  };
+  const clickDot = (index) => {
+    setClicked(true);
+    setChangeIndex(index);
+  };
+
+  return (
+    <div>
+      <div style={{ display: "flex" }}>
+        <div onClick={goPrev}>←</div>
+        {imageArray.map((_, index) => (
+          <div
+            key={index}
+            onClick={() => clickDot(index)}
+            className={styles.dot}
+          ></div>
+        ))}
+        <div onClick={goNext}>→</div>
+      </div>
+      <Image
+        width={640}
+        height={360}
+        alt="project image"
+        src={imageArray[changeIndex].imageUrl}
+      />
+      <p>{imageArray[changeIndex].imageDesc}</p>
+    </div>
+  );
+}
+
+export default Carousel;
--- app/skills/skillscomps/projects.jsx
+++ app/skills/skillscomps/projects.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import styles from "../../page.module.css";
+import StackJson from "../../../stack.json";
+import Wrapper from "../../components/textContState";
+import BtnWrap from "../../components/selectStateBtn";
+import ImageCarousel from "../../components/carousel";
+import { useState } from "react";
 
 function Projects() {
+  const projects = Object.values(StackJson.projects);
+  const [selectProject, setSelectProject] = useState("");
+  const projectFind = Object.values(StackJson.projects).find(
+    (item) => item.title == selectProject
+  );
+
   return (
-    <div>Projects</div>
-  )
+    <>
+      <div className={styles.btnCont2}>
+        {projects.map((item, index) => (
+          <BtnWrap
+            key={index}
+            title={item.title}
+            clickFunc={() => setSelectProject(item.title)}
+          >
+            {item.title}
+          </BtnWrap>
+        ))}
+      </div>
+      {selectProject && (
+        <div className={styles.projectOutputContain}>
+          <h2>{projectFind.title}</h2>
+          <ImageCarousel imageArray={projectFind.image} />
+        </div>
+      )}
+    </>
+  );
 }
 
-export default Projects
+export default Projects;
