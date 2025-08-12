da1b4b3daef54fda1a678dd6dbda0875f66e3315
Nicholas Egner <nick@nicholasegner.com>
2025-08-12 12:04:29 -0500

    layout for photos page

--- app/photos/page.jsx
+++ app/photos/page.jsx
@@ -5,35 +5,6 @@ import styles from "../page.module.css";
 import Link from "next/link";
 import Particles from "../components/particlesBackground";
 
-export const metadata = {
-  title: "Photos of Nicholas Egner (Official Gallery)",
-  description:
-    "Downloadable headshots, portraits, and work images of Minneapolis-based developer Nicholas Egner.",
-  alternates: { canonical: "https://nicholasegner.com/photos" },
-  openGraph: {
-    type: "website",
-    url: "https://nicholasegner.com/photos",
-    title: "Photos of Nicholas Egner",
-    description: "Official photo gallery and press images of Nicholas Egner.",
-    images: [
-      {
-        url: "https://nciholasegner.s3.us-east-2.amazonaws.com/images/nicholas-egner-portfolio.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Nicholas Egner - Official Headshot",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Photos of Nicholas Egner",
-    description: "Official photo gallery and press images of Nicholas Egner.",
-    images: [
-      "https://nciholasegner.s3.us-east-2.amazonaws.com/images/nicholas-egner-portfolio.jpg",
-    ],
-  },
-};
-
 function Photos() {
   const photoObject = [
     {
