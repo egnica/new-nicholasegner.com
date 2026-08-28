a67c97c39aa412fcd13d525fb01ff91979263502
Nicholas Egner <nictacks@gmail.com>
2026-08-28 12:39:39 -0500

    Improve blog image rendering

--- app/components/blocks/Image.jsx
+++ app/components/blocks/Image.jsx
@@ -1,18 +1,26 @@
-import React from "react";
 import Image from "next/image";
-import styles from '../../blog/blog.module.css'
+import styles from "../../blog/blog.module.css";
 
-function ImageG({ block }) {
+export default function ImageBlock({ block }) {
+  if (!block.src) return null;
+
   return (
-    <div className={styles.hero}>
-    <Image
-      src={block.src}
-      alt={block.alt}
-      fill
-      sizes="100vw"
-      style={{ objectFit: "cover" }}
-    /></div>
+    <figure className={styles.contentMediaFigure}>
+      <div className={styles.hero}>
+        <Image
+          src={block.src}
+          alt={block.alt || ""}
+          fill
+          sizes="(max-width: 900px) 100vw, 1200px"
+          style={{ objectFit: "contain" }}
+        />
+      </div>
+
+      {block.caption && (
+        <figcaption className={styles.mediaCaption}>
+          {block.caption}
+        </figcaption>
+      )}
+    </figure>
   );
 }
-
-export default ImageG;
